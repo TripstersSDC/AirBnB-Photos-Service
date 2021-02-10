dde0b4b61cc39a6fcb021bf02a5ccbcff16588a0
Anthony Patterson <pattersonanthony14@gmail.com>
2021-02-09 19:37:22 -0500

    asadfasdf

---
+++ connectionString.js
@@ -0,0 +1,11 @@
+const connection = {
+  user: 'postgres',
+  password: 'ubuntu12345!@#$%',
+  host: '54.144.194.38',
+  dialect: 'postgres',
+  database: 'postgres',
+  port: 5432,
+  max: 200,
+};
+
+module.exports = connection;
